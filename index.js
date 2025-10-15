6a1298d711d315bdba24b1a70b3b991e07f6dfd2
Chris1079 <itssuper1079@gmail.com>
2025-10-14 22:05:11 -0400

    submit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const burgers = ["hamburger", "cheesebruger"];
+const featuredDrink = "Strawberry Milkshake";
+function addBurger() {
+    const newBurger = "Flatburger";
+    burgers.push(newBurger);
+}
+if (true) {
+        const anotherNewBurger = "Maple Bacon Burger";
+        burgers.push(anotherNewBurger);
+    };
+
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";
+}
